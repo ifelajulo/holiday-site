c304314c5734307a86c9e0cd50efce8c33776910
0001SID <mailofsumandas@gmail.com>
2019-08-05 18:59:17 +0530

    initial work

---
+++ js/main.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+
+}
+
+createElement(){
+    data = {
+        place:["Agra, Delhi"],
+        image:["assets/images/test-image.jpg"]
+        theme: ["honeymoon","bech"],
+        activities: ["trekking","adventures"],
+        Btv: ["jfm"],
+        popularity: ["mustVisit"],
+        places: ["goa","delhi"],
+        budget: ["15000"],
+        duration: ["10"]
+    }
+    let element = document.getElementById('products').innerHTML;
+
+
+}
